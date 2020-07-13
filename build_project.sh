6346429ecdd1c746652b3b84ec608ecfea70758d
Corentin Rondier <corentin.rondier@epitech.eu>
2020-07-13 11:49:19 +0200

    Update build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
-cd /github/workspace/ &&
+workdir=/github/workspace
+
+[ -d workdir ] || workdir=./
+
+cd $workdir &&
 make |& tee build_results.txt
 makereturnval=${PIPESTATUS[0]}
 [ $makereturnval -eq 0 ] || exit $makereturnval
